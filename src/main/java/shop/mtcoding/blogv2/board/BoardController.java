4c38edba23bd19d4b2626cff79811e37b30c8272
saki8661 <saki8661@naver.com>
2023-08-11 14:15:57 +0900

    BoardService 글쓰기 구현완료

--- src/main/java/shop/mtcoding/blogv2/board/BoardController.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardController.java
@@ -1,36 +1,29 @@
 package shop.mtcoding.blogv2.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import shop.mtcoding.blogv2.user.User;
 
 @Controller
 public class BoardController {
-
-    private BoardRepository boardRepository;
+    @Autowired
+    private BoardService boardService;
 
     @GetMapping("/board/saveForm")
     public String saveForm() {
         return "board/saveForm";
     }
 
-    // 1. 데이터 받기 (OK)
+    // 1. 데이터 받기 (V)
     // 2. 인증체크 (:TODO)
     // 3. 유효성 검사 (:TODO)
-    // 4. 핵심로직 호출(서비스-트랜잭션관리)
+    // 4. 핵심로직 호출(서비스)
     // 5. view or data 응답
-    @PostMapping("board/save")
+    @PostMapping("/board/save")
     public String save(BoardRequest.SaveDTO saveDTO) {
-        Board board = Board.builder()
-                .title(saveDTO.getTitle())
-                .content(saveDTO.getContent())
-                .user(User.builder().id(1).build())
-                .build();
-
-        boardRepository.save(board);
-
+        boardService.글쓰기(saveDTO, 1);
         return "redirect:/";
     }
 }
